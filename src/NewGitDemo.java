68df4aaaaed99bc8f3f89abb2f1f915415841021
saclauw <saclauw@gmail.com>
2018-01-23 14:17:09 -0500

    first commit to GitPractice

---
+++ src/NewGitDemo.java
@@ -0,0 +1,19 @@
+/*
+ * Steven Clauw
+ * GitPractice assignment
+ */
+import java.util.Scanner;
+public class NewGitDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Please enter your name: ");
+		String userName = scan.nextLine();
+		System.out.println("Hello, " + userName);
+		
+	}
+
+}
